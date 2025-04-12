f4e3e8e86c17f84937f0986442b903e888b81330
MAINOR ANDRES CASTRO VARGAS <MAINOR.CASTRO@ucr.ac.cr>
2025-04-12 12:09:20 -0600

    Agregando primera parte del laboratiorio 4 y modificando la estructura del repositorio

---
+++ backend-lab-c21955/backend-lab-c21955/Controllers/PaisesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_lab_c21955.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hola Mundo";
+        }
+
+    }
+}
